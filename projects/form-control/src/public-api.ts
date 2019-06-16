b409e6387416c19709cdb711b89471724d5fd952
Joster422 <Joster422@gmail.com>
2019-06-16 01:49:36 -0400

    mit license

--- projects/form-control/src/public-api.ts
+++ projects/form-control/src/public-api.ts
@@ -2,3 +2,4 @@ export * from './lib/form-control.service';
 export * from './lib/toggle/toggle.component';
 export * from './lib/small-number/small-number.component';
 export * from './lib/form-control.module';
+export * from './lib/button/button.component';
