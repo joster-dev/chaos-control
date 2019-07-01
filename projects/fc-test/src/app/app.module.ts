251e1bdf0e3aff9574693e91b145cfa75dda66de
Joster422 <Joster422@gmail.com>
2019-06-30 23:44:50 -0400

    fix href

--- projects/fc-test/src/app/app.module.ts
+++ projects/fc-test/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+
 import { FormControlModule } from '@joster/form-control';
+
+import { AppComponent } from './app.component';
 import { ToggleComponent } from './toggle/toggle.component';
 import { SmallNumberComponent } from './small-number/small-number.component';
 import { ButtonComponent } from './button/button.component';
