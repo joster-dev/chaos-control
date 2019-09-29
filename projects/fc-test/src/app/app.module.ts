01a17e26f8c96b00c0000abc3100183309a406a1
Joster422 <Joster422@gmail.com>
2019-09-29 13:31:14 -0400

    refactor - dep loop

--- projects/fc-test/src/app/app.module.ts
+++ projects/fc-test/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { FormControlModule } from '@joster/form-control';
+import { FormControlModule } from 'dist/form-control';
 
 import { AppComponent } from './app.component';
 import { ToggleComponent } from './toggle/toggle.component';
