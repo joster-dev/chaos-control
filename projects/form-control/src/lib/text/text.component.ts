28a48a04f25ef313ca1f2382cb073de05c6b51f4
Joster422 <Joster422@gmail.com>
2020-02-10 00:12:55 -0500

    feat - text smooth grow

--- projects/form-control/src/lib/text/text.component.ts
+++ projects/form-control/src/lib/text/text.component.ts
@@ -1,9 +1,9 @@
-import { Component, forwardRef, Input } from '@angular/core';
+import { Component, forwardRef, Input, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { NG_VALUE_ACCESSOR, NG_VALIDATORS, Validator, ControlValueAccessor, ValidationErrors } from '@angular/forms';
 import { FormControlService } from '../form-control.service';
 
 @Component({
-  selector: 'fc-text[name]',
+  selector: 'fc-text',
   templateUrl: './text.component.html',
   styleUrls: ['./text.component.scss', '../styles.scss'],
   providers: [
@@ -24,27 +24,44 @@ export class TextComponent implements ControlValueAccessor, Validator {
   @Input() nullTitle = this.formControlService.nullTitle;
   @Input() showNull = this.formControlService.showNull;
   @Input() showIcon = true;
-  // name is requied in @Component:selector
-  @Input() name!: string;
+  @Input() showMaxlengthHint = true;
   @Input() label?: string;
-  @Input() placeholder = '';
+  @Input() placeholder?: string;
+  @Input() minlength = 0;
   @Input() maxlength = 100;
   @Input() required = false;
-  // todo: @Input() pattern
+  @Input() replacePattern = /(\r\n|\n|\r)/gm;
+  @Input() autocapitalize: 'none' | 'sentences' | 'words' | 'characters' = 'none';
+  @Input() spellcheck: 'true' | 'false' | 'default' = 'default';
+
+  @ViewChild('textarea', { static: true }) textareaElement!: ElementRef;
+  @ViewChild('textareaHidden', { static: true }) textareaHiddenElement!: ElementRef;
 
   isDisabled = false;
-  error?: 'required' | 'maxlength';
+  textareaHeight = 'auto';
+  error?: 'required' | 'maxlength' | 'minlength';
 
-  constructor(private formControlService: FormControlService) { }
+  constructor(private formControlService: FormControlService, private renderer: Renderer2) { }
 
   _model: string | null = null;
   get model() {
     return this._model;
   }
   set model(value: string | null) {
-    if (value === '') value = null;
-    this._model = value;
+    value = value === null ? '' : value;
+    value = value.replace(this.replacePattern, '');
+    this._model = value === '' ? null : value;
     this.onChange(this._model);
+    setTimeout(() => this.setTextareaHeight());
+  }
+
+  setTextareaHeight() {
+    const textarea = this.textareaElement.nativeElement as HTMLElement;
+    const textareaHidden = this.textareaHiddenElement.nativeElement as HTMLElement;
+    debugger;
+    this.renderer.setStyle(textareaHidden, 'width', `calc(${textarea.scrollWidth}px - 1em)`);
+    this.renderer.setStyle(textareaHidden, 'height', 'auto');
+    this.renderer.setStyle(textarea, 'height', `${textareaHidden.scrollHeight}px`);
   }
 
   onChange(_model: string | null) { }
@@ -62,15 +79,22 @@ export class TextComponent implements ControlValueAccessor, Validator {
   }
 
   validate(): ValidationErrors | null {
-    if (this.model === null) {
-      if (this.required === true) {
-        this.error = 'required';
-        return { required: true };
-      }
-
-      return null;
+    if (this._model === null && this.required === true) {
+      this.error = 'required';
+      return { required: true };
     }
 
+    if (this._model !== null && this._model.length > this.maxlength) {
+      this.error = 'maxlength';
+      return { maxlength: true };
+    }
+
+    if ((this._model === null ? '' : this._model).length < this.minlength) {
+      this.error = 'minlength';
+      return { minlength: true };
+    }
+
+    this.error = undefined;
     return null;
   }
 
