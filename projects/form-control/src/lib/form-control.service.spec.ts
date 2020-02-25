745df62c43d7caa1faabae537d5f2ebbfd73ef7f
Joster422 <Joster422@gmail.com>
2020-02-24 22:08:03 -0500

    update ng

--- projects/form-control/src/lib/form-control.service.spec.ts
+++ projects/form-control/src/lib/form-control.service.spec.ts
@@ -3,10 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { FormControlService } from './form-control.service';
 
 describe('FormControlService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: FormControlService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(FormControlService);
+  });
 
   it('should be created', () => {
-    const service: FormControlService = TestBed.get(FormControlService);
     expect(service).toBeTruthy();
   });
 });
