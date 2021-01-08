c60e744c683fa4f1ec1ea636b8c4962c7a8b7295
Joster422 <Joster422@gmail.com>
2021-01-07 22:01:10 -0500

    file validators

--- projects/form-control/src/lib/file/is-mine.function.ts
+++
@@ -1,8 +0,0 @@
-import { mimeTypes } from './mime-types.const';
-import { mime } from './mime.type';
-
-export function isMimeArray(value: any): value is mime[] {
-  if (!Array.isArray(value))
-    return false;
-  return value.every(item => mimeTypes.includes(item));
-}
