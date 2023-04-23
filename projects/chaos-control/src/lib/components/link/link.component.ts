e64a642ae82052c65c826e744a5e541660c2ca34
Joster422 <Joster422@gmail.com>
2023-04-23 14:43:42 -0400

    feat allow route params

--- projects/chaos-control/src/lib/components/link/link.component.ts
+++ projects/chaos-control/src/lib/components/link/link.component.ts
@@ -11,7 +11,7 @@ import { BorderRadiusDirective } from '../../directives';
   ]
 })
 export class LinkComponent extends BorderRadiusDirective {
-  @Input() link = '';
+  @Input() link: string | string[] = '';
   @Input() href = '';
   @Input() hreflang = '';
   @Input() target: '_self' | '_blank' | '_parent' | '_top' = '_self';
