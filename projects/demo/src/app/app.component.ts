260000b1ff2d0f9ecb23e8d33e2ddcd07fccdb63
joster-dev <joster422@gmail.com>
2026-06-26 22:04:08 -0500

    feat

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { BusyDirective } from 'chaos-control';
+import { BusyDirective, ChoiceComponent } from 'chaos-control';
 import { ContentComponent } from './content/content.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'demo-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     changeDetection: ChangeDetectionStrategy.Eager,
-    imports: [ContentComponent, BusyDirective],
+    imports: [ContentComponent, BusyDirective, ChoiceComponent, FormsModule],
 })
 export class AppComponent {
   theme = 'dark';
-  themes = ['dark', 'light', 'red'];
+  themes = ['dark', 'light', 'red', 'blue', 'yellow'].map((theme) => ({ key: theme, value: theme }));
   promise?: Promise<void>;
   forever = 999999999;
 
@@ -19,7 +20,7 @@ export class AppComponent {
     this.promise = new Promise((r) => window.setTimeout(() => r(), this.forever));
   }
 
-  onClickTheme(theme: string) {
+  onChangeTheme(theme: string) {
     this.theme = theme;
     document.body.setAttribute('data-theme', theme);
   }
